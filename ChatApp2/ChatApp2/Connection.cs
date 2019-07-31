3a3450ef901632ac247e9c74a15c5bb53dbaee8b
tavisca-rpandey <51921974+tavisca-rpandey@users.noreply.github.com>
2019-07-31 10:51:07 +0530

    Completed the Working
    It is the complete Peer to Peer chat App in C#, the first instance starts listening and the second connects to it to start the chat.

---
+++ ChatApp2/ChatApp2/Connection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatApp2
+{
+    public class Connection
+    {
+        private string _ip;
+        private int _port;
+        public Socket ClientSocket, chatSocket;
+
+        public void GetIP()
+        {
+            _ip = "127.0.0.1";
+        }
+        public void GetPort()
+        {
+            _port = 13000;
+        }
+        public void Connect()
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(_ip), _port);
+            socket.Connect(ep);
+            ClientSocket = socket;
+
+        }
+
+        public void StartListening()
+        {
+            chatSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(_ip), _port);
+            chatSocket.Bind(ep);
+            chatSocket.Listen(1);
+            Console.WriteLine("__________Listening____________");
+            ClientSocket = default(Socket);
+            ClientSocket = chatSocket.Accept(); // thread this
+            Console.WriteLine("---------Connected---------");
+        }
+    }
+
+}
---
+++ ChatApp2/ChatApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ChatApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChatApp app = new ChatApp();
+            app.StartApp();
+
+        }
+    }
+
+
+}
